7680faf63b8b7a6ff3906224a56e30fae0e37ec4
lsiwh37249 <lsiwh37249@gmail.com>
2024-07-22 08:59:26 +0900

    init

---
+++ Hello.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+        page_title="Hello"
+
+)
+
+st.write("# Welcome to Hamburger blog!")
+
+st.markdown(
+        """
+        This is my "FIRST" streamlit webpage!
+
+
+        ### See more infos! 
+        [Blog] https://allargandotech.tistory.com/
+        """
+        )
---
+++ README.md
@@ -0,0 +1,4 @@
+### 첫번째 streamlit_project
+
+Hello.py를 웹페이지의 첫화면으로 설정하기 
+
